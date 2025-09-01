fa65c322b48d71cb0270d0fba6476c59bdda0dfb
hornysde <hornysde@proton.me>
2025-08-31 23:48:37 -0700

    Add tool to download url from stdin

---
+++ tools/download_url.py
@@ -0,0 +1,65 @@
+import asyncio
+import hashlib
+import logging
+import sys
+
+import yt_dlp  # type: ignore[import-untyped]
+import pyffmpeg  # type: ignore[import-untyped]
+
+worker_count = 3
+
+
+async def download_worker(queue: asyncio.Queue):
+    ydl_opts = {
+        "ffmpeg_location": pyffmpeg.FFmpeg().get_ffmpeg_bin(),
+        "format": "bestvideo*+bestaudio/best",
+        "quiet": True,
+        "no_warnings": True,
+        "noprogress": True,
+        "concurrent_fragment_downloads": 10,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as dl:
+        while True:
+            url = await queue.get()
+            if url is None:
+                break
+            hash = hashlib.new("md5", url.encode("utf-8")).hexdigest()[:5]
+            dl.params["outtmpl"] = {
+                "default": f"downloads/from_url/%(title)s_{hash}.%(ext)s",
+            }
+            try:
+                await asyncio.to_thread(lambda: dl.download([url]))
+            except Exception as e:
+                print(str(e))
+            queue.task_done()
+            print(f"Remaining: {queue.qsize()}")
+
+
+async def stdin_producer(queue: asyncio.Queue):
+    print("Paste URLs (Ctrl+D to finish):")
+    while True:
+        line = await asyncio.to_thread(sys.stdin.readline)
+        if line == "":
+            break
+        url = line.strip()
+        if url == "":
+            continue
+        await queue.put(url)
+    for _ in range(worker_count):
+        await queue.put(None)
+
+
+async def main():
+    # Shut pyffmpeg up
+    logging.getLogger("pyffmpeg").handlers = []
+
+    queue: asyncio.Queue = asyncio.Queue()
+    workers = [asyncio.create_task(download_worker(queue)) for _ in range(worker_count)]
+    await stdin_producer(queue)
+    print("Waiting for download to complete...")
+    await queue.join()
+    await asyncio.gather(*workers)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
